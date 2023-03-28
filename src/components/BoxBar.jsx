c36e4599c1f5f63963a8be515457f24fc7d322fe
escidron <eduardosanchezcidron@gmail.com>
2023-03-27 23:41:38 -0300

    fix bug product size

--- src/components/BoxBar.jsx
+++ src/components/BoxBar.jsx
@@ -4,7 +4,7 @@ import StackList from './StackList';
 import user1 from '../assets/user1.png'
 import user2 from '../assets/user2.png'
 import user3 from '../assets/user3.png'
-import makeArrow from '../utils/arrows';
+// import makeArrow from '../utils/arrows';
 import MiniProgressBar from './MiniProgressBar';
 
 export default function BoxBar({ top, left, background, porcent, tank, product, product2, plant, barID }) {
@@ -26,13 +26,13 @@ export default function BoxBar({ top, left, background, porcent, tank, product, 
         }
     ]
 
-    const id = setInterval(() => {
-        const c = makeArrow(barID)
-        if (c) {
-            console.log(c)
-            clearInterval(id)
-        }
-    }, 1000);
+    // const id = setInterval(() => {
+    //     const c = makeArrow(barID)
+    //     if (c) {
+    //         console.log(c)
+    //         clearInterval(id)
+    //     }
+    // }, 1000);
 
     return (
         <div className='box-bar' style={{ top: `${top}%`, left: `${left}%` }}>
@@ -40,7 +40,7 @@ export default function BoxBar({ top, left, background, porcent, tank, product, 
             <StackList users={users} background={background} />
 
             {/* <img className='tank' src={require(`../assets/${tank}_galao.gif`)} alt='tank'/> */}
-            <canvas className={`arrow-${barID}`} id={`arrow${barID}`}></canvas>
+            {/* <canvas className={`arrow-${barID}`} id={`arrow${barID}`}></canvas> */}
             <div className='mini-circle-1'>
                 <h1 className='mini-circle-porcent'>12</h1>
             </div>
