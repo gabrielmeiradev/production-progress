9760fc54885c5064b29ef4c150cadb0360a9dbdb
gabrielmeiradev <gabriel_meira10@hotmail.com>
2025-01-23 16:47:02 -0300

    adding slider to stacklist

--- src/components/BoxBar.jsx
+++ src/components/BoxBar.jsx
@@ -1,82 +1,109 @@
-import React, { useState } from 'react'
-import ProgressBar from './ProgressBar';
-import StackList from './StackList';
-import makeArrow from '../utils/arrows';
-import MiniProgressBar from './MiniProgressBar';
+import React, { useState } from "react";
+import ProgressBar from "./ProgressBar";
+import StackList from "./StackList";
+import makeArrow from "../utils/arrows";
+import MiniProgressBar from "./MiniProgressBar";
 
-export default function BoxBar({ top, left, background, porcent, product2, plant, barID, data }) {
-    const [time, setTime] = useState("")
+export default function BoxBar({
+  top,
+  left,
+  background,
+  porcent,
+  product2,
+  plant,
+  barID,
+  data,
+}) {
+  const [time, setTime] = useState("");
+  const convertTimer = (timer) => {
+    const [datePart, timePart] = timer.split(" ");
+    const [year, month, day] = datePart.split("-").map(Number);
+    const [hours, minutes, seconds] = timePart.split(":").map(Number);
 
-    const convertTimer = (timer) => {
-        const [hours, minutes, seconds] = timer.split(':').map(Number);
-        const now = new Date();
-        const endTime = new Date(now.getFullYear(), now.getMonth(), now.getDate(), hours, minutes, seconds);
-        const timeDiff = now - endTime;
+    const endTime = new Date(year, month - 1, day, hours, minutes, seconds);
+    const now = new Date();
 
-        if (timeDiff <= 0) {
-            return "00:00:00";
-        }
+    const timeDiff = now - endTime;
 
-        const diffHours = Math.floor((timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        const diffMinutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
-        const diffSeconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
+    if (timeDiff <= 0) {
+      return "0d 00:00:00";
+    }
 
-        let timeString = `${diffHours.toString().padStart(2, '0')}:${diffMinutes.toString().padStart(2, '0')}:${diffSeconds.toString().padStart(2, '0')}`;
+    const diffDays = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+    const diffHours = Math.floor(
+      (timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    const diffMinutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
+    const diffSeconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
 
-        return timeString;
-    };
+    const timeString = `${diffDays > 0 ? `${diffDays}d ` : ""}${String(
+      diffHours
+    ).padStart(2, "0")}:${String(diffMinutes).padStart(2, "0")}:${String(
+      diffSeconds
+    ).padStart(2, "0")}`;
+    return timeString;
+  };
 
-        if(data.active_since) {
-            setInterval(() => {
-                setTime(convertTimer(data.active_since));
-            }, 1000);
-        }
-
-    const id = setInterval(() => {
-        const c = makeArrow(barID)
-        if (c) {
-            clearInterval(id)
-        }
+  if (data.active_since) {
+    setInterval(() => {
+      setTime(convertTimer(data.active_since));
     }, 1000);
+  }
 
-  
+  const id = setInterval(() => {
+    const c = makeArrow(barID);
+    if (c) {
+      clearInterval(id);
+    }
+  }, 1000);
 
-    return (
-        <div className='box-bar' style={{ top: `${top}%`, left: `${left}%` }}>
-            <ProgressBar porcent={data.total_porcentage} plant={plant} />
+  return (
+    <div className="box-bar" style={{ top: `${top}%`, left: `${left}%` }}>
+      <ProgressBar porcent={data.total_porcentage} plant={plant} />
 
-            <StackList users={data.users} background={background} />
-            
-            <canvas className={`arrow-${barID}`} id={`arrow${barID}`}></canvas>
-            <canvas className={`arrow-${barID}`} id={`arrow-line${barID}`}></canvas>
-            <div className='mini-circle-1'>
-                
-                <h1 className='mini-circle-porcent'>{data.turn_3}</h1>
-            </div>
-            <div className='mini-circle-2'>
-                <h1 className='mini-circle-porcent'>{data.turn_2}</h1>
-            </div>
-            <div className='mini-circle-3'>
-                <h1 className='mini-circle-porcent'>{data.turn_1}</h1>
-            </div>
-            {data.active_since && <div className="timer">{time}</div>}
-           
-            <div className='mini-progress-bar'>
-                <MiniProgressBar />
-               
-                <div className="porcentage-div-mini-bar">
-                    <h1 className="porcentage-mini-bar">{data.product.porcentage}%</h1>
-                </div>
-                <div className='product-name-area'>
-                    <>
-                        <div className='product2-div'>
-                            <img className='product2' src={data.product.image ? data.product.image : require(`../assets/${product2}.png`)} alt='product' />
-                        </div>
-                    </>
+      <StackList users={data.users} background={background} />
 
-                </div>
-            </div>
+      <canvas className={`arrow-${barID}`} id={`arrow${barID}`}></canvas>
+      <canvas className={`arrow-${barID}`} id={`arrow-line${barID}`}></canvas>
+      <div className="mini-circle-1">
+        <h1 className="mini-circle-porcent">{data.turn_3}</h1>
+      </div>
+      <div className="mini-circle-2">
+        <h1 className="mini-circle-porcent">{data.turn_2}</h1>
+      </div>
+      <div className="mini-circle-3">
+        <h1 className="mini-circle-porcent">{data.turn_1}</h1>
+      </div>
+      {data.active_since && (
+        <fieldset className="timer">
+          <legend>Formulando</legend>
+
+          <span>{time}</span>
+        </fieldset>
+      )}
+
+      <div className="mini-progress-bar">
+        <MiniProgressBar />
+
+        <div className="porcentage-div-mini-bar">
+          <h1 className="porcentage-mini-bar">{data.product.porcentage}%</h1>
         </div>
-
-    )
+        <div className="product-name-area">
+          <>
+            <div className="product2-div">
+              <img
+                className="product2"
+                src={
+                  data.product.image
+                    ? data.product.image
+                    : require(`../assets/${product2}.png`)
+                }
+                alt="product"
+              />
+            </div>
+          </>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/StackList.jsx
+++ src/components/StackList.jsx
@@ -1,32 +1,45 @@
-import React from 'react'
+import React, { useState } from "react";
 
-export default function StackList({users,background}) {
+export default function StackList({ users, background }) {
+  const [slide, setSlide] = useState(0);
+
+  const slideChangeInterval = setInterval(() => {
+    nextSlide();
+  }, 3000);
+
+  const nextSlide = () => {
+    clearInterval(slideChangeInterval);
+    if (slide >= users.length / 2 - 1) return setSlide(0);
+    setSlide(slide + 1);
+  };
+  const prevSlide = () => {
+    clearInterval(slideChangeInterval);
+    if (slide <= 0) return;
+    setSlide(slide - 1);
+  };
+  if (users.length <= 0) {
+    return;
+  }
+
   return (
-    <div className={background?'stack-list-bg':'stack-list'}>
-
-        {users[0]?
-            <div id='list-element-1' className='stack-list-element-1'>
-                <img src={users[0].image} alt="user1" className='list-image'/>
-                <h6 className='list-text'>{users[0].name}</h6>
-                <h6 className='list-text'>{users[0].time}</h6>
-            </div>:''
-        }
-
-        {users[1]?
-            <div className='stack-list-element-2'>
-                <img src={users[1].image} alt="user2" className='list-image'/>
-                <h6 className='list-text'>{users[1].name}</h6>
-                <h6 className='list-text'>{users[1].time}</h6>
-            </div>:""
-        }
-        {users[2]?
-            <div className='stack-list-element-fade'>
-                <img src={users[2].image} alt="user3" className='list-image'/>
-                <h6 className='list-text'>{users[2].name}</h6>
-                <h6 className='list-text'>{users[2].time}</h6>
-            </div>:""
-        }
-
+    <div className="stack-wrapper">
+      <div className={`stack-list ${background ? "stack-list-bg" : ""}`}>
+        {users.map((user, index) => (
+          <div
+            key={index}
+            className={`stack-list-element`}
+            style={{ transform: `translateX(${slide * -100}%)` }}
+          >
+            <img
+              src={user.image}
+              alt={`user${index + 1}`}
+              className="list-image"
+            />
+            <h6 className="stack-name">{user.name}</h6>
+            <h6 className="list-text">{user.time}</h6>
+          </div>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
